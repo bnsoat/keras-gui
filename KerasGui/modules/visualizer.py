e954e6d04ca9162cb904f425b29e0f3adcaa728c
bnsoat <bestnightcoresongsofalltime@gmail.com>
2018-07-01 08:08:21 +0300

    add visualizer.py

---
+++ KerasGui/modules/visualizer.py
@@ -0,0 +1,15 @@
+from .layers_gui import *
+from .layers_info import *
+# this class will print visualize layer_gui and layer_info classess
+class Visualizer:
+    def __init__(self):
+        pass
+    #print only layer info without printing the container
+    def visualize_info(self,layer_info):
+        pass
+    #print only layer container without its info
+    def visualize_container(self,layer_gui):
+        pass
+    #print layer container and its info
+    def visualize_layer(self,layer_gui):
+        pass
